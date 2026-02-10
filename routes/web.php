a965f7d22b5c21624ac69a93fc07195fa37339d8
rizki13102008 <aryantor082@example.com>
2026-02-10 09:33:40 +0700

    web php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
-use App\Http\Controllers\BookController;
-Route::resource('books', BookController::class);
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\PeminjamanController;
+
+Route::resource('buku', BukuController::class);
+Route::resource('anggota', AnggotaController::class);
+Route::resource('peminjaman', PeminjamanController::class);
